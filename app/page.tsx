ac40d25fe4725afedde1532d4a5a468477ddfd61
superscalar <65932360+superscalar@users.noreply.github.com>
2024-05-02 19:18:18 -0300

    Initial empty next.js with a front page

---
+++ app/page.tsx
@@ -0,0 +1,22 @@
+
+
+export default function Home() {
+	return (
+		<main id="test">
+			<header><h1>Tienda</h1></header>
+			<section>
+
+				<h2>Grandes productos:</h2>
+				<ul>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+				</ul>
+				
+			</section>
+			<footer>© 2024</footer>
+		</main>
+	);
+}
