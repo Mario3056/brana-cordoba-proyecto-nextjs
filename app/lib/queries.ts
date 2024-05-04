47fc09d0712a091c7a93aba6e6a5bf34c93ab6d6
superscalar <65932360+superscalar@users.noreply.github.com>
2024-05-04 19:49:04 -0300

    Added @vercel/postgres packaged and fixed type bugs in queries.ts

--- app/lib/queries.ts
+++ app/lib/queries.ts
@@ -1,12 +1,13 @@
 import { sql } from '@vercel/postgres';
 import { unstable_noStore as noStore } from 'next/cache';
+import { AdminUser, Product } from '@/definitions'
 
 export async function getUsers(): AdminUser[] {
 	noStore();
 	try {
 		const user = await sql`SELECT * FROM tienda.administradores`;
-		console.log(user.rows);
-		return user.rows[0] as AdminUser;
+		// console.log(user.rows);
+		return user.rows as AdminUser[];
 	} catch (error) {
 		console.error('Failed to fetch admin account:', error);
 		throw new Error('Failed to fetch admin account');
@@ -17,8 +18,8 @@ export async function getProducts(): Product[] {
 	noStore();
 	try {
 		const user = await sql`SELECT * FROM tienda.catalogo`;
-		console.log(user.rows);
-		return user.rows[0] as AdminUser;
+		// console.log(user.rows);
+		return user.rows as Product[];
 	} catch (error) {
 		console.error('Failed to fetch admin account:', error);
 		throw new Error('Failed to fetch admin account');
