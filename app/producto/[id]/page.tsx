90a843c537c3cee2c1218d3b465b322bc9c5de56
Mario Córdoba <cord0388@gmail.com>
2024-05-08 21:33:24 -0300

    missed placeholder image in image src

--- app/producto/[id]/page.tsx
+++ app/producto/[id]/page.tsx
@@ -8,7 +8,7 @@ export default async function Producto({ params }: { params: { id: string } }) {
         <section className="text-gray-600 body-font overflow-hidden">
             <div className="container px-5 py-24 mx-auto">
                 <div className="lg:w-4/5 mx-auto flex flex-wrap">
-                    <img alt="ecommerce" className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded" src="https://images.pexels.com/photos/2582937/pexels-photo-2582937.jpeg"/>
+                    <img alt="ecommerce" className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded" src={product.image}/>
                         <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
                             <h2 className="text-sm title-font text-gray-500 tracking-widest">{product.category}</h2>
                             <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">{product.name}</h1>
