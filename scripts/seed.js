0f8cce40a3ce01fc86e45cfcf6a84188a65028be
superscalar <65932360+superscalar@users.noreply.github.com>
2024-05-10 15:14:27 -0300

    Refactor de los datos para el seed

--- scripts/seed.js
+++ scripts/seed.js
@@ -1,5 +1,6 @@
 const { db } = require('@vercel/postgres');
 const bcrypt = require('bcrypt');
+const productos = require('./seed_data.js');
 
 async function createDB(client) {
 	console.log("--------------------------------");
@@ -36,29 +37,7 @@ async function createAdministrators(client) {
 	console.log("--------------------------------");
 }
 
-let productos = [
-	{ id: 33575, name: 'Producto A', description: 'Un gran producto', category: 'Misc.', price: 819264, rating: 4.4, image: 'products/placeholder.png' },
-	{ id: 33576, name: 'Producto B', description: 'Un gran producto', category: 'Misc.', price: 819264, rating: 4.4, image: 'products/placeholder.png' },
-	{ id: 33577, name: 'Producto C', description: 'Un gran producto', category: 'Misc.', price: 819264, rating: 4.4, image: 'products/placeholder.png' },
-	{ id: 33578, name: 'Producto D', description: 'Un gran producto', category: 'Misc.', price: 819264, rating: 4.4, image: 'products/placeholder.png' },
-	{ id: 33579, name: 'Producto E', description: 'Un gran producto', category: 'Misc.', price: 819264, rating: 4.4, image: 'products/placeholder.png' },
-	{ id: 33580, name: 'Producto F', description: 'Un gran producto', category: 'Misc.', price: 819264, rating: 4.4, image: 'products/placeholder.png' },
-	{ id: 33581, name: 'Producto G', description: 'Un gran producto', category: 'Misc.', price: 819264, rating: 4.4, image: 'products/placeholder.png' },
-	{ id: 33582, name: 'Producto H', description: 'Un gran producto', category: 'Misc.', price: 819264, rating: 4.4, image: 'products/placeholder.png' },
-	{ id: 33583, name: 'Producto I', description: 'Un gran producto', category: 'Misc.', price: 819264, rating: 4.4, image: 'products/placeholder.png' },
-	{ id: 33584, name: 'Producto J', description: 'Un gran producto', category: 'Misc.', price: 819264, rating: 4.4, image: 'products/placeholder.png' },
-	{ id: 33585, name: 'Producto K', description: 'Un gran producto', category: 'Misc.', price: 819264, rating: 4.4, image: 'products/placeholder.png' },
-	{ id: 33586, name: 'Producto L', description: 'Un gran producto', category: 'Misc.', price: 819264, rating: 4.4, image: 'products/placeholder.png' },
-	{ id: 33587, name: 'Producto M', description: 'Un gran producto', category: 'Misc.', price: 819264, rating: 4.4, image: 'products/placeholder.png' },
-	{ id: 33588, name: 'Producto N', description: 'Un gran producto', category: 'Misc.', price: 819264, rating: 4.4, image: 'products/placeholder.png' },
-	{ id: 33589, name: 'Producto O', description: 'Un gran producto', category: 'Misc.', price: 819264, rating: 4.4, image: 'products/placeholder.png' },
-	{ id: 33590, name: 'Producto P', description: 'Un gran producto', category: 'Misc.', price: 819264, rating: 4.4, image: 'products/placeholder.png' },
-	{ id: 33591, name: 'Producto Q', description: 'Un gran producto', category: 'Misc.', price: 819264, rating: 4.4, image: 'products/placeholder.png' },
-	{ id: 33592, name: 'Producto R', description: 'Un gran producto', category: 'Misc.', price: 819264, rating: 4.4, image: 'products/placeholder.png' },
-	{ id: 33593, name: 'Producto S', description: 'Un gran producto', category: 'Misc.', price: 819264, rating: 4.4, image: 'products/placeholder.png' },
-];
-
-async function createProducts(client) {
+async function createProducts(client) {	
 	console.log("--------------------------------");
 	console.log("Creating products table");
 	await client.sql`CREATE TABLE IF NOT EXISTS tienda.catalogo (
---
+++ scripts/seed_data.js
@@ -0,0 +1,23 @@
+const productData = [
+	{ id: 33575, name: 'Producto A', description: 'Un gran producto', category: 'Misc.', price: 819264, rating: 4.4, image: '/products/placeholder.png' },
+	{ id: 33576, name: 'Producto B', description: 'Un gran producto', category: 'Misc.', price: 819264, rating: 4.4, image: '/products/placeholder.png' },
+	{ id: 33577, name: 'Producto C', description: 'Un gran producto', category: 'Misc.', price: 819264, rating: 4.4, image: '/products/placeholder.png' },
+	{ id: 33578, name: 'Producto D', description: 'Un gran producto', category: 'Misc.', price: 819264, rating: 4.4, image: '/products/placeholder.png' },
+	{ id: 33579, name: 'Producto E', description: 'Un gran producto', category: 'Misc.', price: 819264, rating: 4.4, image: '/products/placeholder.png' },
+	{ id: 33580, name: 'Producto F', description: 'Un gran producto', category: 'Misc.', price: 819264, rating: 4.4, image: '/products/placeholder.png' },
+	{ id: 33581, name: 'Producto G', description: 'Un gran producto', category: 'Misc.', price: 819264, rating: 4.4, image: '/products/placeholder.png' },
+	{ id: 33582, name: 'Producto H', description: 'Un gran producto', category: 'Misc.', price: 819264, rating: 4.4, image: '/products/placeholder.png' },
+	{ id: 33583, name: 'Producto I', description: 'Un gran producto', category: 'Misc.', price: 819264, rating: 4.4, image: '/products/placeholder.png' },
+	{ id: 33584, name: 'Producto J', description: 'Un gran producto', category: 'Misc.', price: 819264, rating: 4.4, image: '/products/placeholder.png' },
+	{ id: 33585, name: 'Producto K', description: 'Un gran producto', category: 'Misc.', price: 819264, rating: 4.4, image: '/products/placeholder.png' },
+	{ id: 33586, name: 'Producto L', description: 'Un gran producto', category: 'Misc.', price: 819264, rating: 4.4, image: '/products/placeholder.png' },
+	{ id: 33587, name: 'Producto M', description: 'Un gran producto', category: 'Misc.', price: 819264, rating: 4.4, image: '/products/placeholder.png' },
+	{ id: 33588, name: 'Producto N', description: 'Un gran producto', category: 'Misc.', price: 819264, rating: 4.4, image: '/products/placeholder.png' },
+	{ id: 33589, name: 'Producto O', description: 'Un gran producto', category: 'Misc.', price: 819264, rating: 4.4, image: '/products/placeholder.png' },
+	{ id: 33590, name: 'Producto P', description: 'Un gran producto', category: 'Misc.', price: 819264, rating: 4.4, image: '/products/placeholder.png' },
+	{ id: 33591, name: 'Producto Q', description: 'Un gran producto', category: 'Misc.', price: 819264, rating: 4.4, image: '/products/placeholder.png' },
+	{ id: 33592, name: 'Producto R', description: 'Un gran producto', category: 'Misc.', price: 819264, rating: 4.4, image: '/products/placeholder.png' },
+	{ id: 33593, name: 'Producto S', description: 'Un gran producto', category: 'Misc.', price: 819264, rating: 4.4, image: '/products/placeholder.png' },
+];
+
+module.exports = productData;
