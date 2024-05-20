f7bb1f7cb00268b0d430913a5fbdfc7bc3793646
Mario Córdoba <cord0388@gmail.com>
2024-05-20 14:59:16 -0300

    completing admin products page

--- app/admin/productos/page.tsx
+++ app/admin/productos/page.tsx
@@ -1,9 +1,20 @@
 import Table from '@/app/ui/admin/productos/table';
+import { fetchProductsPages } from '@/app/lib/queries_local';
+import SearchBar from '@/app/ui/catalogo/searchBar';
+import Pagination from '@/app/ui/catalogo/pagination';
 
- export default function Productos() {
+ export default async function Productos({ searchParams }: { searchParams?: { page?: number, query?: string } }) {
+	const query = searchParams?.query || '';
+	const currentPage = Number(searchParams?.page) || 1;
+	const totalPages = await fetchProductsPages(query);
+
 	return (
 		<section className="text-gray-600 body-font">
-			<Table query={""} currentPage={1}/>
+			<SearchBar />
+			<Table query={query} currentPage={currentPage}/>
+			<div className="mb-5 mt-5 flex w-full justify-center">
+        		<Pagination totalPages={totalPages} />
+      		</div>
 		</section>
 	);
  }
