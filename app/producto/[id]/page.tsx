be740d041e9aa3d2cbcda9258a5d942cf9038b6c
Mario Córdoba <cord0388@gmail.com>
2024-06-08 16:53:32 -0300

    components separation

--- app/producto/[id]/page.tsx
+++ app/producto/[id]/page.tsx
@@ -1,11 +1,44 @@
 import { getProductById } from '@/app/lib/queries_local'
 import Card from '@/app/ui/product/card';
+import CommentCard from '@/app/ui/product/comments/card';
+import { ProductComment } from '@/app/lib/types';
+import AddNewCommentForm from '@/app/ui/product/comments/addNewForm';
 
 export default async function Producto({ params }: { params: { id: string } }) {
     const product = await getProductById(params.id);
+    const comentario: ProductComment = {
+        name: "The Catalyzer",
+        rating: 2,
+        comment: "Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine."
+    }
+
     return (
         <>
-            <Card product={product}/>
+            <Card product={product} />
+            
+            <section className="text-gray-600 body-font">
+                <div className="container pt-5 pb-15 py-5 mx-auto">
+                    <div className="flex flex-col text-center w-full mb-20">
+                        <h1 className="sm:text-3xl text-2xl font-medium title-font text-gray-900">Comentarios</h1>
+
+                    </div>
+                    <div className="flex flex-wrap -m-4">
+                        <CommentCard comment={comentario} />   
+                        <CommentCard comment={comentario} />
+                        <CommentCard comment={comentario} />                     
+                    </div>
+                </div>
+            </section>
+
+            <section className="text-gray-600 body-font relative">
+                <div className="container px-5 py-24 mx-auto">
+                    <div className="flex flex-col text-center w-full mb-12">
+                        <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Agregar comentario</h1>
+                        <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Nos encantaría conocer tu opinión. Por favor, comparte tus comentarios y opiniones sobre el producto.</p>
+                    </div>
+                    <AddNewCommentForm/>
+                </div>
+            </section>
         </>
     )
 }
---
+++ app/ui/product/comments/card.tsx
@@ -0,0 +1,26 @@
+import StarRating from '@/app/ui/starRating';
+import { ProductComment } from "@/app/lib/types";
+
+export default function CommentCard({comment}: {comment: ProductComment}) {
+    return (
+        <>
+            <div className="p-4 md:w-1/3">
+                <div className="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
+                    <div className="flex items-center mb-3">
+                        <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-indigo-500 text-white flex-shrink-0">
+                            <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
+                                <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
+                                <circle cx="12" cy="7" r="4"></circle>
+                            </svg>
+                        </div>
+                        <h2 className="text-gray-900 text-lg title-font font-medium">{comment.name}</h2>
+                    </div>
+                    <StarRating rating={comment.rating} />
+                    <div className="flex-grow mt-2">
+                        <p className="leading-relaxed text-base">{comment.comment}</p>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
