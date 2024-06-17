a6f243757e2f6b3d6e2a0349b8cd2d149a3fdc78
Mario Córdoba <cord0388@gmail.com>
2024-06-17 17:10:44 -0300

    added empty cart message

--- app/carrito/page.tsx
+++ app/carrito/page.tsx
@@ -11,37 +11,50 @@ export default function Carrito() {
 
   return (
     <section>
-      <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
-        <div className="mx-auto max-w-3xl">
-          <header className="text-center">
-            <h1 className="text-xl font-bold text-gray-900 sm:text-3xl">
-              Carrito
-            </h1>
-          </header>
+      {(carrito.length === 0) ? (
+        <div className="grid h-screen place-content-center bg-white px-4">
+          <div className="text-center">
+            <h1 className="text-9xl font-black text-gray-200">Ups!</h1>
 
-          <div className="mt-8">
-            <ul className="space-y-4">
-              {carrito.map((product: CartProduct) => (
-                <li key={product.id} className="flex items-center gap-4">
-                  <Producto product={product} />
-                </li>
-              ))}
-            </ul>
+            <p className="text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">Carrito vacío</p>
 
-            <div className="mt-8 flex justify-end border-t border-gray-100 pt-8">
-              <div className="w-screen max-w-lg space-y-4">
-                <div className="space-y-0.5 text-sm text-gray-700 flex justify-between font-medium">
-                  <span>Total</span>
-                  <span>{"$" + currentCarritoPrice / 100}</span>
+            <p className="mt-4 text-gray-500">Visita nuestro catálogo para agregar productos a tu carrito.</p>
+
+          </div>
+        </div>
+      ) : (
+        <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
+          <div className="mx-auto max-w-3xl">
+            <header className="text-center">
+              <h1 className="text-xl font-bold text-gray-900 sm:text-3xl">
+                Carrito
+              </h1>
+            </header>
+
+            <div className="mt-8">
+              <ul className="space-y-4">
+                {carrito.map((product: CartProduct) => (
+                  <li key={product.id} className="flex items-center gap-4">
+                    <Producto product={product} />
+                  </li>
+                ))}
+              </ul>
+
+              <div className="mt-8 flex justify-end border-t border-gray-100 pt-8">
+                <div className="w-screen max-w-lg space-y-4">
+                  <div className="space-y-0.5 text-sm text-gray-700 flex justify-between font-medium">
+                    <span>Total</span>
+                    <span>{"$" + currentCarritoPrice / 100}</span>
+                  </div>
+
+                  <MercadoPagoButton />
+
                 </div>
-
-                <MercadoPagoButton />
-
               </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
     </section>
   );
 }
