da788bc1c9e9bd5d57818b68bead91a7e3f853e4
superscalar <65932360+superscalar@users.noreply.github.com>
2024-06-23 13:11:21 -0300

    Fix bug with product deletion modal

--- app/ui/admin/productos/buttons.tsx
+++ app/ui/admin/productos/buttons.tsx
@@ -7,10 +7,14 @@ import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 
 function disableAfterOneClick(event: any) {
+	console.log("disableAfterOneClick:", event.target);
 	if ( event.currentTarget.classList.contains("disabled")) {
 		event.preventDefault();
 	} else {
+	/*
 		event.currentTarget.classList.add("disabled");
+		event.currentTarget.classList.add("text-blue-400");
+	*/
 	}
 }
 
@@ -46,12 +50,21 @@ export function RestoreDeletedProductButton({ id }: { id: string }) {
 	);
 }
 
+function disableModalAndDelete(id: string, event: any) {
+	disableAfterOneClick(event);
+
+	// console.log(event.target);
+	// const product_id = event.target.getAttribute("modalid");
+	const modal: HTMLDialogElement | null = document.getElementById("delete_modal" + id) as HTMLDialogElement;
+	if (modal != null) modal.close();
+}
+
 export function DeleteProductButton({ id }: { id: string }) {
 	const deleteProductWithId = deleteProduct.bind(null, id);
-
+	const deleteButtonClickHandler = disableModalAndDelete.bind(null, id);
 	return (
 		<form action={deleteProductWithId}>
-			<button onClick={disableAfterOneClick}
+			<button onClick={deleteButtonClickHandler}
 				className="py-2 leading-none px-3 font-medium text-red-600 hover:text-red-500 duration-150 hover:bg-gray-50 rounded-lg">
 				
 				Eliminar
@@ -62,19 +75,20 @@ export function DeleteProductButton({ id }: { id: string }) {
 
 export function CloseModalButton({modal_id}: {modal_id: string}) {
 	return <button onClick={(event) => {
-									const modal: HTMLDialogElement | null = document.getElementById(modal_id) as HTMLDialogElement;
-									if (modal != null) modal.close();
-								}}
-				className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+					const modal: HTMLDialogElement | null = document.getElementById(modal_id) as HTMLDialogElement;
+					if (modal != null) modal.close();
+				}}
+					className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
 				X
 			</button>;
 }
 
 export function TriggerDeleteConfirmModal({modal_id}: {modal_id: string}) {
 	return <button onClick={(event) => {
-								const modal: HTMLDialogElement | null = document.getElementById(modal_id) as HTMLDialogElement;
-								if (modal != null) modal.showModal();
-							}}
+					// console.log(event.target, "------>", modal_id);
+					const modal: HTMLDialogElement | null = document.getElementById(modal_id) as HTMLDialogElement;
+					if (modal != null) modal.showModal();
+				}}
 					className="py-2 leading-none px-3 font-medium text-red-600 hover:text-red-500 duration-150 hover:bg-gray-50 rounded-lg">
 				Eliminar
 			</button>;
